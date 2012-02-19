f1e28e1df847a3f1c40925045c3299c5226cb5c2
Evan Jones <evan_t_jones@mac.com>
2012-02-19 12:33:28 -0800

    added dem_extractor.h

---
+++ web/cgi-bin/dem_extractor.h
@@ -0,0 +1,38 @@
+#ifndef DEM_EXTRACTOR_H
+#define DEM_EXTRACTOR_H
+
+#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+class LatLng{
+public:
+    float lat;
+    float lng;
+    LatLng( float lat_in, float lng_in);
+    LatLng();
+
+};
+
+class LatLngRegion{
+// I think it's probably tacky to make everything public like this. 
+public:
+    float min_lat, max_lat, min_lng, max_lng;
+    LatLng min_lat_lng, max_lat_lng;
+    
+    bool contains_point( LatLng lat_lng);
+    bool contains_region( LatLngRegion other_region);
+    LatLngRegion( LatLng min_lat_lng_in, LatLng max_lat_lng_in);
+    
+};
+
+class DemRegion{
+  public:
+      LatLngRegion region;
+      int lat_samples;
+      int lng_samples;
+      signed short *buf;  
+      DemRegion( LatLngRegion region_in);
+};
+#endif /* end of include guard: DEM_EXTRACTOR_H */
+
+
